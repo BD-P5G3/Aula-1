1b9137f1836c8c51fbcb362a1237c44d44c04125
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-02-20 00:35:20 +0000

    Hide text in password textbox

--- Aula 1/Form1.Designer.cs
+++ Aula 1/Form1.Designer.cs
@@ -43,37 +43,37 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(25, 17);
+            this.label1.Location = new System.Drawing.Point(29, 23);
             this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 21);
+            this.label1.Size = new System.Drawing.Size(191, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "SQL server address";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(25, 42);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(29, 56);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(644, 23);
+            this.textBox1.Size = new System.Drawing.Size(735, 27);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(25, 142);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Location = new System.Drawing.Point(29, 189);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(644, 23);
+            this.textBox2.Size = new System.Drawing.Size(735, 27);
             this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(25, 95);
+            this.label2.Location = new System.Drawing.Point(29, 127);
             this.label2.Margin = new System.Windows.Forms.Padding(0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 21);
+            this.label2.Size = new System.Drawing.Size(154, 28);
             this.label2.TabIndex = 2;
             this.label2.Text = "Authentication";
             // 
@@ -81,10 +81,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(25, 124);
+            this.label3.Location = new System.Drawing.Point(29, 165);
             this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.Size = new System.Drawing.Size(46, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Login";
             // 
@@ -92,27 +92,27 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(25, 178);
+            this.label4.Location = new System.Drawing.Point(29, 237);
             this.label4.Margin = new System.Windows.Forms.Padding(0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(57, 15);
+            this.label4.Size = new System.Drawing.Size(70, 20);
             this.label4.TabIndex = 6;
             this.label4.Text = "Password";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(25, 196);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Location = new System.Drawing.Point(29, 261);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(644, 23);
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(735, 27);
             this.textBox3.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(206, 247);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(235, 329);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 34);
+            this.button1.Size = new System.Drawing.Size(158, 45);
             this.button1.TabIndex = 7;
             this.button1.Text = "Test connection";
             this.button1.UseVisualStyleBackColor = true;
@@ -120,10 +120,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(350, 247);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(400, 329);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 34);
+            this.button2.Size = new System.Drawing.Size(158, 45);
             this.button2.TabIndex = 8;
             this.button2.Text = "Get table content";
             this.button2.UseVisualStyleBackColor = true;
@@ -131,10 +130,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(700, 302);
+            this.ClientSize = new System.Drawing.Size(800, 403);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
@@ -144,7 +143,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Aula 1";
             this.ResumeLayout(false);
